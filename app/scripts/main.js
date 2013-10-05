9fac5cb577c4f716dfcaef8dd4aa4904b60d2d19
Nathan Smith <nsmith13@nd.edu>
2013-10-04 23:32:45 -0400

    integrated changes from original development

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
-console.log('\'Allo \'Allo!');
+/**
+ * Event Dispatcher
+ *
+ * Events:
+ *     * moveMade returns number removed
+ *     * gameOver
+ *     * preGameOver
+ *     * postGameOver
+ *     * newGame
+ *     * preNewGame
+ *
+ */
+dispatcher = _.extend({}, Backbone.Events);
+
+
+
+
+var SliderGameApp = Backbone.View.extend({
+    rows: 4, cols: 4,
+    initialize: function(options) {
+        this.collection = new SliderGameCollection({height: this.rows, width: this.cols})
+        this.collection.generateRandomMatrix();
+
+        this.display = new CanvasDisplayView({
+			el: this.el,
+            collection: this.collection,
+            rows: this.rows, cols: this.cols,
+			drawCellCallback: _.bind(this.drawCell, this),
+        });
+
+    },
+	drawCell: function(x, y, cell, d) {
+		d.$el.drawRect({
+			fillStyle: '#FFF',
+			x: x+d._CELL_PADDING, y: y+d._CELL_PADDING,
+            width: d._CELL_WIDTH, height: d._CELL_HEIGHT,
+            fromCenter: false
+		})
+	}
+});
+
+
+
+game = new SliderGameApp({el: '#game_canvas'});
---
+++ app/scripts/utils.js
@@ -0,0 +1,11 @@
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function boolToInt(val) {
+    return val ? 1 : 0;
+}
